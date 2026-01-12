f02a73a3ced473d492a3f8cae7bb44ad8e0b3ad9
Damon <dong765@qq.com>
2026-01-12 18:20:45 +0800

    U

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,22 +1,26 @@
+import app from 'flarum/forum/app'; // 确保路径与 webpack externals 一致
 import { extend } from 'flarum/common/extend';
-import app from 'flarum/forum/app';
 import Post from 'flarum/forum/components/Post';
 
-app.initializers.add('damonhu-biosignature', () => {
-  extend(Post.prototype, 'footer', function(items) {
-    const post = this.attrs.post;
-    if (!post) return;
+if (!app) {
+  console.error('Flarum app 对象未找到！请检查 Webpack externals 配置。');
+} else {
+  app.initializers.add('damonhu-biosignature', () => {
+    extend(Post.prototype, 'footer', function (items) {
+      const post = this.attrs.post;
+      if (!post) return;
 
-    const user = post.user();
-    // 这里的 .attribute('bio') 对应 extend.php 里定义的名称
-    const bio = user ? user.attribute('bio') : null;
+      const user = post.user();
+      // 这里的 .attribute('bio') 对应 extend.php 里定义的名称
+      const bio = user ? user.attribute('bio') : null;
 
-    if (bio) {
-      items.add('bio-signature', (
-        <div className="Post-signature">
-          {bio}
-        </div>
-      ), -50); // -50 确保它在最后面
-    }
+      if (bio) {
+        items.add('bio-signature', (
+          <div className="Post-signature">
+            {bio}
+          </div>
+        ), -50); // -50 确保它在最后面
+      }
+    });
   });
-});
+}
