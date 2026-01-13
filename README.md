31b866bbf00cc38be243ab27e9d71f405ce49010
Damon <dong765@qq.com>
2026-01-13 10:38:19 +0800

    u

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # flarum-ext-biosignature
+
+Flarum plugin: display the bio in the post signature area.
+
+flarum插件，将bio显示在帖子签名处
+
+![](./demo.png)
+
+### install：
+
+```
+composer require damonhu/flarum-ext-biosignature:*
+```
+
+### update
+
+```
+composer update damonhu/flarum-ext-biosignature:*
+php flarum cache:clear
+flarum assets:publish
+```
+
--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,31 +1,15 @@
 // import app from 'flarum/forum/app'; // 确保路径与 webpack externals 一致
 import { extend } from 'flarum/common/extend';
-import Post from 'flarum/forum/components/Post';
 import CommentPost from 'flarum/forum/components/CommentPost';
 
-if (!app) {
-  console.error('Flarum app 对象未找到！请检查 Webpack externals 配置。');
-} else {
-  console.error('开始执行初始化器');
-  app.initializers.add('flarum-ext-biosignature', () => {
-    console.info("init")
-    extend(Post.prototype, 'content', function (items) {
-      console.error("content")
-    })
-    extend(Post.prototype, 'view', function (items) {
-      console.error("view")
-    })
+app.initializers.add('flarum-ext-biosignature', () => {
     extend(CommentPost.prototype, 'footerItems', function (items) {
-      console.error("ssssssss")
       const post = this.attrs.post;
-      console.error("ssssssss1", post)
       if (!post) return;
-
       const user = post.user();
       // 这里的 .attribute('bio') 对应 extend.php 里定义的名称
       const bio = user ? user.attribute('bio') : null;
 
-      console.error("ssssssss2", user, bio)
       if (bio) {
         items.add('bio-signature', (
           <div className="Post-signature">
@@ -34,5 +18,4 @@ if (!app) {
         ), -50); // -50 确保它在最后面
       }
     });
-  });
-}
+});
