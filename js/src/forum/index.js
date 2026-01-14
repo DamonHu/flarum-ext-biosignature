8ac157500f3006b906711f75724fb3054f1c13ca
Damon <dong765@qq.com>
2026-01-14 10:32:01 +0800

    U

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -11,10 +11,9 @@ app.initializers.add('flarum-ext-biosignature', () => {
       const bio = user ? user.attribute('bio') : null;
 
       if (bio) {
+        console.log(bio)
         items.add('bio-signature', (
-          <div className="Post-signature" dangerouslySetInnerHTML={{ __html: bio }}>
-            {/* {bio} */}
-          </div>
+          <div className="Post-signature" dangerouslySetInnerHTML={bio}></div>
         ), -50); // -50 确保它在最后面
       }
     });
